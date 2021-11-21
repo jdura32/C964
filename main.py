feba3ad5e5bbde8f669a06fe45ae6ab898834156
Jake Durand <jakedurand00@gmail.com>
2021-11-21 15:51:14 -0800

    Remove package that's not found

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Load libraries
 from pandas import read_csv
-from pandas.plotting import scatter_matrix
+# from pandas.plotting import scatter_matrix
 from matplotlib import pyplot
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
